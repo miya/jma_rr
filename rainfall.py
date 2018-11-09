8de3221803d068a487340a312d50bf7d09566f45
m0zu <m0zurillex@gmail.com>
2018-11-09 21:38:25 +0900

    時間データ処理方法の変更&make_git()の簡略化

--- rainfall.py
+++ rainfall.py
@@ -1,36 +1,29 @@
 import requests,glob,datetime,os
 from PIL import Image
-from bs4 import BeautifulSoup as bs4
-
 
 def get_image():
-    time_data = (datetime.datetime.now() - datetime.timedelta(days=1)).strftime('%Y%m%d')
-    for h in [str(i).zfill(2) for i in range(24)]:
-        for m in [str(i*5).zfill(2) for i in range(12)]:
-            time = time_data + h + m
-            url = 'http://www.jma.go.jp/jp/radnowc/imgs/radar/000/' + time + '-00.png'
-            req = requests.get(url)
-            if req.status_code == 404:
-                print('Error --> {}'.format(url))
-            else:
-                with open(time + '.png', "wb") as w:
-                    w.write(req.content)
-                    print('Downloaded --> {}'.format(url))
+    time1 = (datetime.datetime.now() - datetime.timedelta(days=(1)))
+    time2 = datetime.datetime(year=time1.year, month=time1.month, day=time1.day)
+    time3 = [(time2 + datetime.timedelta(minutes=(5*i))).strftime('%Y%m%d%H%M') for i in range(288)]
+    for i in time3:
+        url = 'http://www.jma.go.jp/jp/radnowc/imgs/radar/000/{}-00.png'.format(i)
+        req = requests.get(url)
+        if req.status_code == 200:
+            with open(i + '.png', "wb") as w:
+                w.write(req.content)
+                print('Downloaded --> {}'.format(url))
+        else:
+            print('Error --> {}'.format(url))
     print('[complete] get_image')
-
 
 def make_gif():
     file_name = (datetime.datetime.now() - datetime.timedelta(days=1)).strftime('%Y-%m-%d') + '.gif'
-    files = sorted(glob.glob('./*.png'))
-    images = list(map(lambda file: Image.open(file), files))
+    files = sorted(glob.glob('*.png'))
+    images = [Image.open(i) for i in files]
     images[0].save(file_name, save_all=True, append_images=images[1:], duration=50, loop=0)
-    [os.remove(f) for f in glob.glob('*.png')]
+    [os.remove(f) for f in files]
     print('[complete] make_gif')
-
 
 if __name__ == '__main__':
     get_image()
     make_gif()
-
-
-
