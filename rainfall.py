0fa220205c80411eebcbcb2d7f1e957df0ad9acb
m0zu <m0zurillex@gmail.com>
2018-12-26 12:01:38 +0900

    ダウンロード中の出力をtqdmに変更

--- rainfall.py
+++ rainfall.py
@@ -2,21 +2,18 @@ import os
 import glob
 import datetime
 import requests
+from tqdm import tqdm
 from PIL import Image
 
 def get_image():
     time1 = (datetime.datetime.now() - datetime.timedelta(days=(1)))
     time2 = datetime.datetime(year=time1.year, month=time1.month, day=time1.day)
     time3 = [(time2 + datetime.timedelta(minutes=(5*i))).strftime('%Y%m%d%H%M') for i in range(288)]
-    for i in time3:
-        url = 'http://www.jma.go.jp/jp/radnowc/imgs/radar/000/{}-00.png'.format(i)
+    for i in tqdm(range(288)):
+        url = 'http://www.jma.go.jp/jp/radnowc/imgs/radar/000/{}-00.png'.format(time3[i])
         req = requests.get(url)
-        if req.status_code == 200:
-            with open(i + '.png', "wb") as w:
-                w.write(req.content)
-                print('Downloaded --> {}'.format(url))
-        else:
-            print('Error --> {}'.format(url))
+        with open(time3[i] + '.png', "wb") as w:
+            w.write(req.content)
     print('[complete] get_image')
 
 def make_gif():
