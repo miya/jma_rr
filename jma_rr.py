1d796020afacac4d714c188727f0dd0af4815d57
m0zu <m0zurillex@gmail.com>
2019-01-10 15:23:31 +0900

    画像ダウンロード処理をmultiprocessingで行う

--- jma_rr.py
+++ jma_rr.py
@@ -1,33 +1,39 @@
 import os
+import time
 import glob
 import dropbox
 import requests
-from tqdm import tqdm
 from PIL import Image
+from multiprocessing import Pool
 from datetime import datetime, timedelta, timezone
 
-'''画像取得メソッド
-気象庁のレーダー・ナウキャスト(https://www.jma.go.jp/jp/radnowc/)にアクセスし、前日の1日分の画像(5分毎にアップロードされるので5*12=288)をダウンロードします。
-`'''
-def get_image():
-    time1 = (datetime.now(JST) - timedelta(days=(1)))
+'''時間データをセットするメソッド
+降水画像データを取得するため
+'''
+def set_timeData():
+    time1 = (datetime.now(jst) - timedelta(days=(1)))
     time2 = datetime(year=time1.year, month=time1.month, day=time1.day)
     time3 = [(time2 + timedelta(minutes=(5*i))).strftime('%Y%m%d%H%M') for i in range(288)]
-    for i in tqdm(range(288)):
-        url = 'http://www.jma.go.jp/jp/radnowc/imgs/radar/000/{}-00.png'.format(time3[i])
-        req = requests.get(url)
-        with open(time3[i] + '.png', "wb") as w:
+    return time3
+
+'''画像取得メソッド
+気象庁のレーダー・ナウキャスト(https://www.jma.go.jp/jp/radnowc/)にアクセスし、前日の1日分の観測画像(5分毎にアップロードされるので5*12=288)をダウンロードします。
+ダウンロードはmultiprocessingを使い並行処理で行います。
+`'''
+def get_image(parts):
+    url = 'http://www.jma.go.jp/jp/radnowc/imgs/radar/000/{}-00.png'.format(parts)
+    req = requests.get(url)
+    with open(parts + '.png', "wb") as w:
             w.write(req.content)
-    print('[complete] get_image')
 
 '''gif作成メソッド
-画像取得メソッドのダウンロードした画像をPillowを使いgifを作成します。
+画像取得メソッドでダウンロードした画像をPillowを使いgifを作成します。
 gif作成後使用した画像は全て削除します。
 '''
 def make_gif():
     files = sorted(glob.glob('*.png'))
     images = [Image.open(i) for i in files]
-    images[0].save(FILE_NAME, save_all=True, append_images=images[1:], duration=50, loop=0)
+    images[0].save(file_name, save_all=True, append_images=images[1:], duration=50, loop=0)
     [os.remove(f) for f in files]
     print('[complete] make_gif')
 
@@ -38,13 +44,19 @@ def make_gif():
 def send_dropbox():
     access_token = os.environ.get('DROPBOX_ACCESS_TOKEN')
     dbx = dropbox.Dropbox(access_token)
-    with open(FILE_NAME, 'rb') as f:
-        dbx.files_upload(f.read(), '/' + FILE_NAME)
+    with open(file_name, 'rb') as f:
+        dbx.files_upload(f.read(), '/' + file_name)
     print('[complete] send_dropbox')
 
+'''メイン処理
+TravisCIのタイムゾーンがUTCなのでJSTに変更。
+'''
 if __name__ == '__main__':
-    JST = timezone(timedelta(hours=+9), 'JST')
-    FILE_NAME = (datetime.now(JST) - timedelta(days=1)).strftime('%Y-%m-%d') + '.gif'
-    get_image()
+    jst = timezone(timedelta(hours=+9), 'JST')
+    file_name = (datetime.now(jst) - timedelta(days=1)).strftime('%Y-%m-%d') + '.gif'
+    timeData = set_timeData()
+    with Pool(processes=10) as p:
+        p.map(get_image, timeData)
+    print('[complete] get_image')
     make_gif()
     send_dropbox()
